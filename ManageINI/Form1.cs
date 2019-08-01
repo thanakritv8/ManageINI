65ceba5f09c9d887af080e675dad5b4a5e939b66
Thanakrit.J <Thanakrit.J@roki-thailand.com>
2019-08-01 13:38:24 +0700

    init

---
+++ ManageINI/Form1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ManageINI
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            var MyIni = new IniFile("Settings.ini");
+            MyIni.Write("DefaultVolume", "100", "Audio");
+            MyIni.Write("DefaultVolume", "100", "Audio1");
+            MyIni.DeleteKey("DefaultVolume", "Audio");
+            MyIni.DeleteSection("Audio");
+            var DefaultVolume = MyIni.Read("DefaultVolume", "Audio1");
+            var a = 0;
+        }
+    }
+}
